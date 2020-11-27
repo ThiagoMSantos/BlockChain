cb1ebec210ae8ab9a50ee40253e85aa77f9abd40
Thiago <thiagomathias98@gmail.com>
2020-11-26 22:26:52 -0300

    add InfosMsg

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
-const linkConnection = () =>{
-  if(process.env.DB_URL != "")
-    return process.env.DB_URL
-  else
-    return 'mongodb://localhost/db_blockchain'
-}
-
-mongoose.connect(linkConnection(), {useNewUrlParser:true});
+mongoose.connect((process.env.BASE_URL) ? process.env.DB_URL : 'mongodb://localhost/db_blockchain', {useNewUrlParser:true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
