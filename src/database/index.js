c82261e51fe11e10fc23b2df5f4e7fdca6cc0674
Thiago <thiagomathias98@gmail.com>
2020-11-26 23:28:15 -0300

    att Variavel DB_URL

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect((process.env.BASE_URL) ? process.env.DB_URL : 'mongodb://localhost/db_blockchain', {useNewUrlParser:true});
+mongoose.connect((process.env.DB_URL) ? process.env.DB_URL : 'mongodb://localhost/db_blockchain', {useNewUrlParser:true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
