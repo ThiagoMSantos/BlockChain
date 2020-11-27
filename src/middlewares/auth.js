cc506405ff31aae8a152724e1b6b462a917c1fc2
Thiago <thiagomathias98@gmail.com>
2020-11-27 04:14:25 -0300

    remove Status

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,21 +5,21 @@ module.exports = (req, res, next) =>{
   const authHeader = req.headers.authorization;
 
   if (!authHeader)
-    return res.status(401).send({error: 'Token não informado.'})
+    return res.send({error: 'Token não informado.'})
 
   // Bearer + HASH
   const parts = authHeader.split(' ');
 
   if(!parts.length === 2 )
-    return res.status(401).send({error: 'Token inválido.'});
+    return res.send({error: 'Token inválido.'});
 
   const [scheme, token] = parts;
 
   if(!/^Bearer$/i.test(scheme))
-    return res.status(401).send({error: 'Formato do token inválido.'});
+    return res.send({error: 'Formato do token inválido.'});
 
   jwt.verify(token, authConfig.secret, (err, decoded) =>{
-    if (err) return res.status(401).send({error: 'Token inválido.'})
+    if (err) return res.send({error: 'Token inválido.'})
 
     req.userId = decoded.id;
     return next();
